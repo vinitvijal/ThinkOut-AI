fee89c88982bb95d5a9bbe7e65d8029cf1aa841c
vineet <vineet.22566@sscbs.du.ac.in>
2025-04-05 04:09:31 +0530

    server action for db addition done

--- actions/server.ts
+++ actions/server.ts
@@ -1,5 +1,8 @@
 'use server'
 
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 import { openai } from '@ai-sdk/openai'
 import { generateText } from 'ai';
 import prompts from "@/app/prompts.json"
@@ -8,16 +11,42 @@ export interface catType {
     cat: "promotional-mail" | "informal-mail" | "outreach-mail" | "professional-linkedin" | "casual-linkedin" | "story-linkedin" | "thought-linkedin"
 }
 
+async function addPromptToDB(prompt: string, category: string, response: string, userId: string, token: number, model: string, type: string) {
+    const res = await prisma.response.create({
+        data: {
+            prompt: prompt,
+            category: category,
+            response: response,
+            userId: userId,
+            tokens: token,
+            model: model,
+            type: type,
+        },
+    })
 
-export async function getResponse(prompt: string, category: catType['cat']) {
+    return res
+}
+
+
+
+export async function getResponse(prompt: string, category: catType['cat'], userId?: " " ) {
     
     var textTemplate = prompts[category]
 
-    const { text } = await generateText({
+    const { text, usage} = await generateText({
       model: openai('gpt-4o-mini'),
       prompt: (textTemplate).replace("{context}", prompt) + " \n\n Answer in the same language as the input. \n\n Make sure to remove styling and formatting from the output. ",
     });
-    
+
+
+    const tokens = usage.totalTokens
+    const model = 'gpt-4o-mini'
+    const type = category.split("-")[0]
+    const cat = category.split("-")[1]
+    const response = await addPromptToDB(prompt, cat, text, userId || "NA", tokens, model, type)
+    console.log(response)
+    console.log("Response added to DB")
+
     return text
    
 }
